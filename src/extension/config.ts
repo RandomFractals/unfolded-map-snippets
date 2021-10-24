4820b3c21f1288e664781e8fe66563d1bfa3d93a
RandomFractals <RandomFractals@users.noreply.github.com>
2021-10-24 16:21:15 -0500

    add intro to unfolded map sdk notebook and config (#16)

--- src/extension/config.ts
+++ src/extension/config.ts
@@ -1,38 +1,8 @@
 // built-in notebook examples
 export const notebookExamples: Array<any> = [
 	{
-		name: 'Chicago Speed Cameras',
-		file: './notebooks/chicago-speed-cameras-net.ipynb',
-		type: '.NET Interactive Notebook',
-	},
-	{
-		name: 'Chicago Red Light Cameras',
-		file: './notebooks/chicago-red-light-cameras.ipynb',
-		type: 'Pyolite Notebook',
-	},
-	{
-		name: 'Chicago Red Light Cameras Geo',
-		file: './notebooks/chicago-red-light-cameras-geo.ipynb',
-		type: 'Pyolite Notebook',
-	},
-	{
-		name: 'USA Airports',
-		file: './notebooks/usa-airports-ts.ipynb',
-		type: 'TypeScript Notebook',
-	},
-	{
-		name: 'USA Airports Geo',
-		file: './notebooks/usa-airports-net.ipynb',
-		type: '.NET Interactive Notebook',
-	},
-	{
-		name: 'USA State Capitals',
-		file: './notebooks/usa-state-capitals-ts.ipynb',
-		type: 'TypeScript Notebook',
-	},
-	{
-		name: 'Data Table Viewer',
-		file: './notebooks/data-table-viewer.ojs',
-		type: 'Observable JS Notebook',
+		name: 'Introduction to Unfolded Map SDK',
+		file: './notebooks/unfolded-map-sdk-intro.ipynb',
+		type: 'Python Jupyter Notebook',
 	},
 ];
